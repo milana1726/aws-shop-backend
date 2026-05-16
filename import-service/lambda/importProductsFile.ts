277219a8d4cf9bfb028527a0d52cdd2916e02bd5
Liudmyla Melnychuk <liudmyla_melnychuk@epam.com>
2026-05-16 21:02:35 +0200

    feat: add ImportProductsFile lambda

---
+++ import-service/lambda/importProductsFile.ts
@@ -0,0 +1,48 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const s3 = new S3Client({ region: process.env.AWS_REGION });
+
+const BUCKET = process.env.BUCKET!;
+const UPLOAD_PREFIX = "uploaded/";
+const SIGNED_URL_EXPIRATION = 60;
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const fileName = event.queryStringParameters?.name;
+  const key = `${UPLOAD_PREFIX}${fileName}`;
+
+  if (!fileName) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Query parameter 'name' is required" }),
+    };
+  }
+  try {
+    const command = new PutObjectCommand({
+      Bucket: BUCKET,
+      Key: key,
+    });
+
+    const signedUrl = await getSignedUrl(s3, command, {
+      expiresIn: SIGNED_URL_EXPIRATION,
+    });
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: signedUrl,
+    };
+  } catch (error) {
+    console.error("❌ Error generating signed URL:", error);
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({ message: "Internal Server Error" }),
+    };
+  }
+};
--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,6 +1,10 @@
 import * as cdk from "aws-cdk-lib/core";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as path from "path";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -22,6 +26,45 @@ export class ImportServiceStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       autoDeleteObjects: true,
+    });
+
+    const importProductsFile = new NodejsFunction(this, "ImportProductsFile", {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      entry: path.join(__dirname, "../lambda/importProductsFile.ts"),
+      handler: "handler",
+      functionName: "ImportProductsFile",
+      environment: {
+        BUCKET: importServiceBucket.bucketName,
+      },
+    });
+
+    importServiceBucket.grantPut(importProductsFile);
+
+    const api = new apigateway.RestApi(this, "ImportApi", {
+      restApiName: "Import Service",
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+      },
+    });
+
+    const importResource = api.root.addResource("import");
+    importResource.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(importProductsFile),
+      {
+        requestParameters: {
+          "method.request.querystring.name": true,
+        },
+      },
+    );
+
+    new cdk.CfnOutput(this, "ImportApiUrl", {
+      value: api.url,
+    });
+
+    new cdk.CfnOutput(this, "ImportBucketName", {
+      value: importServiceBucket.bucketName,
     });
   }
 }
