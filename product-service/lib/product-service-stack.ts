1fd0e953fc6d13eceb4aa791a8a0dde23c9898a6
Liudmyla Melnychuk <liudmyla_melnychuk@epam.com>
2026-05-23 20:53:14 +0200

    init: start task 6

--- product-service/lib/product-service-stack.ts
+++ product-service/lib/product-service-stack.ts
@@ -5,6 +5,9 @@ import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as path from "path";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
