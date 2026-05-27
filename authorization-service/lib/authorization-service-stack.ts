2c1b844e9d9b7cc3a1ced9b7ba371ce4e138d2f5
Liudmyla Melnychuk <liudmyla_melnychuk@epam.com>
2026-05-28 00:47:28 +0200

    feat: implement CDK stack and export authorizer ARN

--- authorization-service/lib/authorization-service-stack.ts
+++ authorization-service/lib/authorization-service-stack.ts
@@ -1,16 +1,41 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as path from "path";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 export class AuthorizationServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const githubLogin = process.env.GITHUB_LOGIN;
+    const testPassword = process.env.TEST_PASSWORD;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AuthorizationServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    if (!githubLogin || !testPassword) {
+      throw new Error(
+        "GITHUB_LOGIN or TEST_PASSWORD environment variables are not set. Please add them to authorization-service/.env file!",
+      );
+    }
+
+    const basicAuthorizer = new NodejsFunction(this, "BasicAuthorizer", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      entry: path.join(__dirname, "../lambda/basicAuthorizer.ts"),
+      handler: "handler",
+      functionName: "basicAuthorizer",
+
+      environment: {
+        [githubLogin]: testPassword,
+      },
+    });
+
+    basicAuthorizer.grantInvoke(
+      new iam.ServicePrincipal("apigateway.amazonaws.com"),
+    );
+
+    new cdk.CfnOutput(this, "BasicAuthorizerArn", {
+      value: basicAuthorizer.functionArn,
+      exportName: "BasicAuthorizerArn",
+    });
   }
 }
